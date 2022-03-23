d2b7a1852b0448a73c4cbc6569575edfb12f6932
pedroddvo <pedroddvo@gmail.com>
2022-03-23 07:41:11 +0000

    Initial Commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import * as sphereShader from "./shader/sphereShader.js";
 import * as THREE from './three.js';
+import { OrbitControls } from './OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -8,28 +9,38 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+
 const clock = new THREE.Clock();
 
-const geometry = new THREE.SphereGeometry(5);
-const material = new THREE.ShaderMaterial({
-  uniforms: {
-    time: { value: clock.getElapsedTime() }
-  },
-
-  vertexShader: sphereShader.vertex,
-  fragmentShader: sphereShader.fragment,
+const geometry = new THREE.SphereGeometry(5, 64, 64);
+const texture = new THREE.TextureLoader().load('texture/2k_earth_daymap.jpg');
+const material = new THREE.MeshBasicMaterial({
+  map: texture
 });
 const sphere = new THREE.Mesh(geometry, material);
 scene.add(sphere);
 
+const atmosphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5.1, 64, 64),
+  new THREE.ShaderMaterial({
+    transparent: true,
+    vertexShader: sphereShader.vertex,
+    fragmentShader: sphereShader.fragment,
+  })
+);
+atmosphere.opacity = 0.2;
+
+scene.add(atmosphere);
+
 camera.position.z = 10;
 
 function animate() {
-  sphere.material.uniforms.time.value = clock.getElapsedTime();
-  sphere.rotateY(0.01);
+  sphere.rotateY(0.005);
 
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
+// example text
 
 animate();
--- js/shader/sphereShader.js
+++ js/shader/sphereShader.js
@@ -1,17 +1,22 @@
 export const vertex = `
 varying vec3 vUv;
+varying vec3 vNormal;
 void main(void) {
   vUv = position;
 
   vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
   gl_Position = projectionMatrix * modelViewPosition;
+  vNormal = normalize(normalMatrix * normal);
 }
 `;
 
 export const fragment = `
-uniform float time;
 varying vec3 vUv;
+varying vec3 vNormal;
+
 void main(void) {
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  float intensity = 1.05 - dot(vNormal, vec3(0.0, 0.0, 1.0));
+  vec3 atmosphere = vec3(1.0, 1.0, 1.0) * pow(intensity * 1.5, 3.0);
+  gl_FragColor = vec4(vec3(0.0, 1.2, 2.0) * atmosphere, 0.5);
 }
 `;
